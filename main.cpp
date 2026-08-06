598090a2002bbf001baab1364fe43566ae7039b5
henrique <henrickbalieiro@gmail.com>
2026-08-06 11:32:01 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char** argv) {
+	
+	float a,b,c;
+	
+	a = 8;
+	b = 19;
+	c = a/b;
+	
+	printf("a soma de %d+%d=%d",a,b,c);
+	
+	return 0;
+}
