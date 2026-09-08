f32acd63a0e668fa24babc312a146473c43ba872
henrique <henrickbalieiro@gmail.com>
2026-09-08 17:21:27 -0300

    Add comments to explain calculations and variables
    The code now includes comments explaining variable types and functions used for calculations.

--- (aula2) raio.c
+++ (aula2) raio.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
+#include <stdio.h>                  //é um comando que inclui uma biblioteca no seu programa.
 #include <stdlib.h>
-#include <math.h>
+#include <math.h>                   //biblioteca matematica 
 #define pi 3.14
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) {  //inicio programa 
 	
-	float area, raio;
-	printf("Insira o valor do raio:");
-	scanf("%f", &raio);
-	
-	area = M_PI* pow(raio,2);
-	area = pi*(raio*raio);
+	float area, raio;                             //float(%f): é um tipo de variável usado para armazenar números com casas decimais EX 0.1
+	printf("Insira o valor do raio:");            //int(%d): tipo de variável usado para armazenar números inteiros EX 1 
+	scanf("%f", &raio);                           //printf: é um comando usado para mostrar informações na tela.
+	                                              //scanf: é usado para receber uma informação que o usuário digita no teclado
+	area = M_PI* pow(raio,2);                     //&: quando você usa scanf, ele serve para informar o endereço da variável na memória, para que o scanf possa colocar o valor digitado nela
+	area = pi*(raio*raio);                        //pow: função da biblioteca math.h usada para fazer potências
 	
 	
 	
